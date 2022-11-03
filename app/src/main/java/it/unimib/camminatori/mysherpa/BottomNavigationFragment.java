ffd8fc5c461b87fd23fea513b7afba27bc4dccf1
mattiapezzotti <matpezzo01@gmail.com>
2022-11-03 14:33:17 +0100

    Added bottom navigation bar fragment

--- app/src/main/java/it/unimib/camminatori/mysherpa/BottomNavigationFragment.java
+++ app/src/main/java/it/unimib/camminatori/mysherpa/BottomNavigationFragment.java
@@ -1,22 +1,20 @@
 package it.unimib.camminatori.mysherpa;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SearchBarFragment extends Fragment {
+public class BottomNavigationFragment extends Fragment {
 
-    public SearchBarFragment() {
-        // Required empty public constructor
+    public static BottomNavigationFragment newInstance(String param1, String param2) {
+        BottomNavigationFragment bottomNavigationFragment = new BottomNavigationFragment();
+        return bottomNavigationFragment;
     }
 
-    public static SearchBarFragment newInstance() {
-        SearchBarFragment searchBarFragment = new SearchBarFragment();
-        return searchBarFragment;
+    public BottomNavigationFragment() {
+        // Required empty public constructor
     }
 
     @Override
@@ -27,7 +25,7 @@ public class SearchBarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_bar, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_bottom_navigation, container, false);
+        return rootView;
     }
 }
